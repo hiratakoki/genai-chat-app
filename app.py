61fc388e686ded60f4a185925a15a8cd664a7fe4
unknown <koki.hirata.270@baycurrent.co.jp>
2026-04-16 15:52:46 +0900

    Step3： チャットUI追加

--- app.py
+++ app.py
@@ -14,17 +14,34 @@ client = OpenAI(
 )
 
 st.title("streamlitアプリ練習")
-user_input = st.text_input("質問を入力してください")
+user_input = st.chat_input("質問を入力してください")
 
-if st.button("送信"):
-    st.write("あなたの質問：" + user_input)
+# session_stateの初期化(if文で「まだ無ければ作る」)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# 履歴の表示（for文でループ）
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):  # "user" or "assistant"
+        st.write(msg["content"])
+
+if user_input:
+    # ユーザーの入力を追加
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    # ユーザーのメッセージをその場で表示
+    with st.chat_message("user"):
+        st.write(user_input)
     with st.spinner("回答を生成中..."):
         try:
             response = client.chat.completions.create(
                 model = "google/gemma-3-4b-it:free",
-                messages = [{"role": "user", "content": user_input}],
+                messages = st.session_state.messages    #履歴全体を渡す
             )
             answer = response.choices[0].message.content
-            st.write("質問に対する回答：" + answer)
+            # AIの回答を追加
+            st.session_state.messages.append({"role": "assistant", "content": answer})
+            # AIの回答をその場で表示
+            with st.chat_message("assistant"):
+                st.write(answer)
         except Exception as e:
             st.error(f"エラーが発生しました: {e}")
